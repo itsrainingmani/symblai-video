fed98275e7e62d40699ad957f9a25452593be1cd
Manikandan Sundararajan <mani@hey.com>
2021-12-20 23:01:53 -0500

    handle input onchange

--- components/protectedPage.js
+++ components/protectedPage.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Button, Input, Stack } from '@chakra-ui/react';
 import Header from './header';
 
 const ProtectedPage = ({ children }) => {
 	const isLoggedIn = false;
+	const [appId, setAppId] = useState('');
+	const [appSecret, setAppSecret] = useState('');
 
 	return (
 		<>
@@ -11,8 +13,18 @@ const ProtectedPage = ({ children }) => {
 			{!isLoggedIn ? (
 				<Container>
 					<Stack spacing={3} marginBottom='1rem'>
-						<Input placeholder='appId' size='md' />
-						<Input placeholder='appSecret' size='md' />
+						<Input
+							placeholder='appId'
+							size='md'
+							value={appId}
+							onChange={(e) => setAppId(e.target.value)}
+						/>
+						<Input
+							placeholder='appSecret'
+							size='md'
+							value={appSecret}
+							onChange={(e) => setAppSecret(e.target.value)}
+						/>
 					</Stack>
 					<Button>Login</Button>
 				</Container>
